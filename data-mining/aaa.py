03561bf0e86197b420cebc815b0e084fda7c20cd
yunyunjiang <jyybioyt@gmail.com>
2019-11-11 15:50:50 -0500

    my first commit

--- data-mining/aaa.py
+++ data-mining/aaa.py
@@ -17,6 +17,9 @@ print(y)
 df2=df[['category_id','views','likes','dislikes','comment_count']]
 print(df2.head())
 
+#########################################
+### check correlation among features ####
+#########################################
 
 # plot heatmap #
 
@@ -27,8 +30,10 @@ import matplotlib.pyplot as plt
 
 # Get the correlation matrix, where each entry is the Pearson product-moment correlation coefficient
 cm = np.corrcoef(df2.T)
+print(cm)
 
-plt.figure(figsize=(25, 25))
+
+plt.figure(figsize=(10, 10))
 
 hm = sns.heatmap(cm,
                  cbar=True,
@@ -36,8 +41,51 @@ hm = sns.heatmap(cm,
                  square=True,
                  fmt='.2f',
                  annot_kws={'size': 15},
-                 yticklabels=df.columns,
-                 xticklabels=df.columns)
+                 yticklabels=df2.columns,
+                 xticklabels=df2.columns)
 
 plt.tight_layout()
 plt.show()
+
+
+
+#########################################################
+### handling categorical features in the combined data ##
+#########################################################
+
+
+# Print the unique value and their number for each feature
+for j in range(df.shape[1]):
+    print(df.columns[j] + ':')
+    print(df.iloc[:, j].value_counts(), end='\n\n')
+
+#categorical feature checker #
+
+def categorical_feature_checker(df, target, dtype):
+    """
+    The categorical feature checker
+
+    Parameters
+    ----------
+    df : dataframe
+    target : the target
+    dtype : the type of the feature
+
+    Returns
+    ----------
+    The categorical features and their number of unique value
+    """
+
+    feature_number = [ [ feature, df [ feature ].nunique () ]
+                       for feature in df.columns
+                       if feature != target and df [ feature ].dtype.name == dtype ]
+
+    print ( '%-30s' % 'Categorical feature', 'Number of unique value' )
+    for feature, number in sorted ( feature_number, key = lambda x: x [ 1 ] ):
+        print ( '%-30s' % feature, number )
+
+    return feature_number
+
+feature_number = categorical_feature_checker(df2, 'views', 'object')
+
+print(feature_number)
