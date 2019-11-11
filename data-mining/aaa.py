c7782bf00d398f986a265954f7dde0c3d7f0886f
yunyunjiang <jyybioyt@gmail.com>
2019-11-11 15:15:36 -0500

    my first commit

---
+++ data-mining/aaa.py
@@ -0,0 +1,43 @@
+import pandas as pd
+
+
+# read in the dataset #
+df= pd.read_csv('/Users/yunyunjiang/Documents/machine learning I/project/USvideos.csv',
+                       header=0)
+
+# check variable names #
+print(list(df.columns.values))
+
+# print first 5 observations #
+print(df.head())
+
+y=(1/3)*df['views']+(1/3)*df['likes']+(1/3)*df['dislikes']
+print(y)
+
+df2=df[['category_id','views','likes','dislikes','comment_count']]
+print(df2.head())
+
+
+# plot heatmap #
+
+import numpy as np
+import seaborn as sns
+#%matplotlib inline
+import matplotlib.pyplot as plt
+
+# Get the correlation matrix, where each entry is the Pearson product-moment correlation coefficient
+cm = np.corrcoef(df2.T)
+
+plt.figure(figsize=(16, 16))
+
+hm = sns.heatmap(cm,
+                 cbar=True,
+                 annot=True,
+                 square=True,
+                 fmt='.2f',
+                 annot_kws={'size': 15},
+                 yticklabels=df.columns,
+                 xticklabels=df.columns)
+
+plt.tight_layout()
+plt.show()
